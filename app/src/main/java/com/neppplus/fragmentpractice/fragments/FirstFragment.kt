9e8bbf70b9e57e71061a6931159dc486a1f0da13
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-23 22:48:33 +0900

    첫 프래그먼트 동작 처리 + 메인화면 xml 배치

--- app/src/main/java/com/neppplus/fragmentpractice/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragmentpractice/fragments/FirstFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.neppplus.fragmentpractice.R
 
 class FirstFragment:Fragment() {
@@ -15,6 +18,26 @@ class FirstFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//    }(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//
+//        val changeTextBtn = view.findViewById<Button>(R.id.changeTextBtn)
+//    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val changeTextBtn = view.findViewById<Button>(R.id.changeTextBtn)
+
+        changeTextBtn.setOnClickListener{
+            //임시로->텍스트뷰 문구를 안녕하세요 라고 변경
+            val contentTxt = view.findViewById<TextView>(R.id.contentTxt)
+            contentTxt.text="안녕하세요"
+        }
     }
 }
 
